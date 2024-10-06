38d27bcee20c8e98e44d7870a797b3bc2c3cc5b9
Rui Ferreira <ruif.sps@gmail.com>
2024-10-06 15:28:27 +0100

    Parte 016

--- Fantasy/Fantasy.Shared/Entities/Team.cs
+++ Fantasy/Fantasy.Shared/Entities/Team.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Fantasy.Shared.Resources;
+using System.ComponentModel.DataAnnotations;
 
 namespace Fantasy.Shared.Entities;
 
@@ -6,8 +7,9 @@ public class Team
 {
     public int Id { get; set; }
 
-    [MaxLength(100)]
-    [Required]
+    [Display(Name = "Team", ResourceType = typeof(Literals))]
+    [MaxLength(100, ErrorMessageResourceName = "MaxLength", ErrorMessageResourceType = typeof(Literals))]
+    [Required(ErrorMessageResourceName = "RequiredField", ErrorMessageResourceType = typeof(Literals))]
     public string Name { get; set; } = null!;
 
     public string? Image { get; set; }
